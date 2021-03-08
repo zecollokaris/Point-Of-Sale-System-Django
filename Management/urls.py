6f596805a1008e167badca742bdeb7a984fb3863
zecollokaris <collo.kariss@gmail.com>
2021-03-08 17:28:29 +0300

    added add tems view

--- Management/urls.py
+++ Management/urls.py
@@ -1,10 +1,44 @@
+from django.urls import path
+''' 
+This is to allow us to connect the PROJECT's  url with this APP's url 
+it will then allow us to define paths here as we had included (Management urls)
+in the PROJECT.
+'''
+
 from django.conf.urls import url,include
-from . import views
+
+from Management import views
+'''
+Here we are importing the views as we shall connect templates from there 
+'''
+
+
 from django.contrib import admin
+
 from django.conf.urls.static import static
+
 from django.conf import settings
 
 urlpatterns = [
-    url(r'^$', views.index, name= 'index'),
+
+
+
+#################################################################################################################################################################################
+#URL FOR  HOME PAGE
+#################################################################################################################################################################################
+
+    #HOME Page url!
+
+    #This is the home page url pattern 
+    path('', views.index, name= 'index'),
+
+#################################################################################################################################################################################
+#URL FOR LIST OF ITEMS PAGE VIEW FUNCTION
+#################################################################################################################################################################################
+
+    #LIST OF ITEMS Page url!
+
+    #This is the List of Items page url pattern 
+    path('list_items', views.list_items, name='list_items'),
 ]
 
--- Management/views.py
+++ Management/views.py
@@ -1,3 +1,56 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import *
+
+from .forms import StockCreateForm 
+
+
+#---------------------------------------------------------------------#
+'''End Of Import'''
+#---------------------------------------------------------------------#
+
+# VIEW FUNCTIONS HERE!
+
+
+
+#################################################################################################################################################################################
+#HOME PAGE VIEW FUNCTION
+#################################################################################################################################################################################
+
+#Home page view function
+def index(request):
+	title = 'Welcome: This is the Home Page'
+	context = {
+	"title": title,
+	}
+	return render(request, "Management/index.html",context)
+
+#################################################################################################################################################################################
+#LIST OF ITEMS PAGE VIEW FUNCTION
+#################################################################################################################################################################################
+
+#List of Items page view function
+def list_items(request):
+    title = 'List of list_items'
+    # Queryset to get Stock Objects from Database
+    queryset = Stock.objects.all()
+    context = {
+	"title": title,
+    "queryset": queryset,
+	}
+    return render(request, "Management/list_items.html",context)
+
+#################################################################################################################################################################################
+#ADD ITEMS PAGE VIEW FUNCTION
+#################################################################################################################################################################################
+
+#Add Items page view function
+def add_items(request):
+	form = StockCreateForm(request.POST or None)
+	if form.is_valid():
+		form.save()
+	context = {
+		"form": form,
+		"title": "Add Item",
+	}
+	return render(request, "add_items.html", context)

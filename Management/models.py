debd6bd82c38a71623f02ccf489fbbf90ef644e7
zecollokaris <collo.kariss@gmail.com>
2021-03-09 14:19:05 +0300

    created nav bar and added links to Nav Bar

--- Management/models.py
+++ Management/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 # Stock models here.
 class Stock(models.Model):
-	category = models.CharField(max_length=50, blank=True, null=True)
-	item_name = models.CharField(max_length=50, blank=True, null=True)
-	quantity = models.IntegerField(default='0', blank=True, null=True)
+	category = models.CharField(max_length=50, blank=False, null=True)
+	item_name = models.CharField(max_length=50, blank=False, null=True)
+	quantity = models.IntegerField(default='0', blank=False, null=True)
 	receive_quantity = models.IntegerField(default='0', blank=True, null=True)
 	receive_by = models.CharField(max_length=50, blank=True, null=True)
 	issue_quantity = models.IntegerField(default='0', blank=True, null=True)

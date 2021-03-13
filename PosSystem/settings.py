5a3c2f7a3e76b0a05533517cedd717982d30f231
zecollokaris <collo.kariss@gmail.com>
2021-03-13 15:07:38 +0300

    deleted migration files and connected psq db

--- PosSystem/settings.py
+++ PosSystem/settings.py
@@ -78,8 +78,10 @@ WSGI_APPLICATION = 'PosSystem.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'PSale',
+        'USER': 'karis',
+        'PASSWORD': 'K@r!s##!',
     }
 }
 
